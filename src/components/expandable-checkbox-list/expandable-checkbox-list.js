b7ec8dc4b6cb896270282ef4c30f70229c601e5d
kampaku <43931529+kampaku@users.noreply.github.com>
2020-11-28 02:33:01 +0300

    add expandable-checkbox-list

---
+++ src/components/expandable-checkbox-list/expandable-checkbox-list.js
@@ -0,0 +1,9 @@
+export default function checkboxListOpen() {
+  let btn = document.querySelector('.checkbox__wrapper');
+  let list = document.querySelector('.checkbox-list');
+  let img = document.querySelector('.checkbox-button');
+  btn.addEventListener('click', () => {
+    list.classList.toggle('checkbox-list--active');
+    img.classList.toggle('checkbox-button--active');
+  })
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Main js file
 import buttonLike from '../components/button-like/button-like';
 import dropdown from '../components/dropdown/dropdown';
+import checkboxListOpen from '../components/expandable-checkbox-list/expandable-checkbox-list';
 buttonLike();
 dropdown();
+checkboxListOpen();
