6bc229ddbc0ba90ed33e8eaf396531f586d1b4d9
kampaku <43931529+kampaku@users.noreply.github.com>
2020-12-12 20:02:52 +0300

    add search room page

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,7 +9,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devServer: {
     contentBase: baseWebpackConfig.externals.paths.dist,
     port: 8081,
-    openPage: ['registration.html'],
+    openPage: ['search-room.html'],
     overlay: {
       warnings: true,
       errors: true
