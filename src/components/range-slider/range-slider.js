fed4f0f9e57e77823408a8294724c5e137950c05
kampaku <43931529+kampaku@users.noreply.github.com>
2020-12-31 15:07:43 +0300

    fix range slider

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -5,25 +5,27 @@ import 'nouislider/distribute/nouislider.css';
 let rangeSlider = document.querySelector('.range-slider');
 let value = document.querySelector('.range-slider__value');
 
-noUiSlider.create(rangeSlider, {
-  start: [5000, 10000],
-  behaviour: 'drag',
-  step: 1000,
-  connect: true,
-  range: {
-    'min': 0,
-    'max': 15000
-  },
-  format: wNumb({
-    decimals: 0,
-    thousand: ' ',
-    suffix: '₽'
-  })
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [5000, 10000],
+    behaviour: 'drag',
+    step: 1000,
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 15000
+    },
+    format: wNumb({
+      decimals: 0,
+      thousand: ' ',
+      suffix: '₽'
+    })
+    
+  });
   
-});
-
-rangeSlider.noUiSlider.on('update', function (values) {
-  value.innerHTML = values.join(' - ');
-});
+  rangeSlider.noUiSlider.on('update', function (values) {
+    value.innerHTML = values.join(' - ');
+  });
+}
 
 export default rangeSlider;
