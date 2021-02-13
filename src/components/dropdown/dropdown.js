b8cdb49c38b017121caf305dd5e3d79a4ba78310
kampaku <43931529+kampaku@users.noreply.github.com>
2021-02-13 18:52:01 +0300

    add dropdown js

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,42 +1,154 @@
-const dropdownType = {
-guests : [{
-  name: "взрослые",
-  value: 0,
-},{
-  name: "дети",
-  value: 0,
-},{
-  name: "младенцы",
-  value: 0,
-},]
+function getDeclension(n, wordForms) {
+  n = Math.abs(n) % 100;
+  let n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return wordForms[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return wordForms[1];
+  }
+  if (n1 == 1) {
+    return wordForms[0];
+  }
+  return wordForms[2];
 }
 
-export default class Dropdown {
-  constructor(dropdown) {
-    this.btn = dropdown.querySelector('.dropdown__button');
-    this.list = dropdown.querySelector('.dropdown__inner');
-    this.type = dropdown.querySelector('[data-dropdown-type]');
-    this.storage = Object.assign({}, dropdownType[this.type.dataset.dropdownType]);
+class Dropdown {
+  constructor(dropdown, dropdownType) {
+    this.dropdown = dropdown;
+    this.dropdownType = dropdownType;
+    this.openBtn;
+    this.list;
+    this.listItems;
+    this.buttonHandler = this.buttonHandler.bind(this);
+    this.itemAmount;
+    this.clearButton;
+    this.applyButton;
+    this.clear = this.clear.bind(this);
+    this.apply = this.apply.bind(this);
   }
 
-  
+  getItem(itemElement) {
+    const itemType = itemElement.dataset.type;
+    let item = this.dropdownType.items.find((type) => type.name === itemType);
+    return item;
+  }
 
-  init() {
-    console.log(this.storage);
+  buttonHandler(e) {
+    const target = e.target.closest('.dropdown__item-button');
+    if (target) {
+      this.calculateItem(target);
+      const sum = this.itemsAmount();
+      if (sum === 0) {
+        this.clearButton.hidden = 'true';
+      } else {
+        this.clearButton.hidden = '';
+      }
+      this.changeText(sum);
+    }
+  }
 
-    if (this.btn) {
-      this.btn.addEventListener('click', (e) => {
-        e.preventDefault();
-        this.list.classList.toggle('dropdown__inner--active');
-      })
+  changeText(sum) {
+    let textValue = [];
+    const dropdownText = this.dropdown.querySelector('.dropdown__text');
+
+    if (this.dropdownType.wordForms && sum > 0) {
+      textValue.push(`${sum} ${getDeclension(sum, this.dropdownType.wordForms)}`);
     }
 
-    this.list.querySelector('.dropdown__item-button').addEventListener('click', (e)=>{
+    this.dropdownType.items.forEach((item) => {
+      if (item.wordForms && item.value > 0) {
+        textValue.push(`${item.value} ${getDeclension(item.value, item.wordForms)}`);
+      }
+    });
 
-      this.storage[0].value +=1;
-      console.log(this.storage);
-    })
+    dropdownText.value = textValue.join(', ');
   }
-  
-  
+
+  calculateItem(target) {
+    const itemElement = target.closest('li');
+    const itemAmount = itemElement.querySelector('.dropdown__item-amount');
+    const item = this.getItem(itemElement);
+
+    if (target.dataset.button === 'plus') {
+      item.value += 1;
+    }
+
+    if (target.dataset.button === 'minus') {
+      item.value -= 1;
+    }
+
+    itemAmount.textContent = item.value;
+
+    this.checkNegativeValue(itemElement);
+  }
+
+  itemsAmount() {
+    const sum = this.dropdownType.items.reduce((acc, curr) => curr.value + acc, 0);
+    return sum;
+  }
+
+  checkNegativeValue(itemElement) {
+    const btnMinus = itemElement.querySelector('.dropdown__item-button--minus');
+    const item = this.getItem(itemElement);
+
+    if (item.value === 0) {
+      btnMinus.disabled = 'true';
+    } else {
+      btnMinus.disabled = '';
+    }
+  }
+
+  clear() {
+    const itemAmounts = this.list.querySelectorAll('.dropdown__item-amount');
+    this.dropdownType.items.forEach((item) => (item.value = 0));
+    itemAmounts.forEach((amount) => (amount.textContent = 0));
+    this.changeText();
+    this.listItems.forEach((item) => {
+      this.checkNegativeValue(item);
+    });
+    this.clearButton.hidden = 'true';
+  }
+
+  apply() {
+    this.list.classList.remove('dropdown__inner--active');
+  }
+
+  render() {
+    this.listItems.forEach((item) => {
+      const itemAmount = item.querySelector('.dropdown__item-amount');
+      itemAmount.textContent = this.getItem(item).value;
+    });
+    const sum = this.itemsAmount();
+    if (sum === 0) {
+      this.clearButton.hidden = 'true';
+    }
+    this.changeText(sum);
+  }
+
+  init() {
+    this.openBtn = this.dropdown.querySelector('.dropdown__button');
+    this.list = this.dropdown.querySelector('.dropdown__inner');
+    this.clearButton = this.dropdown.querySelector('.js-button-clear');
+    this.applyButton = this.dropdown.querySelector('.js-button-apply');
+    this.listItems = this.list.querySelectorAll('.dropdown__item');
+
+    if (this.openBtn) {
+      this.openBtn.addEventListener('click', () => {
+        this.list.classList.toggle('dropdown__inner--active');
+      });
+    }
+
+    this.render();
+
+    this.listItems.forEach((item) => {
+      this.checkNegativeValue(item);
+    });
+
+    this.list.addEventListener('click', this.buttonHandler);
+    this.clearButton.addEventListener('click', this.clear);
+    this.applyButton.addEventListener('click', this.apply);
+  }
 }
+
+export { Dropdown };
--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,32 @@
 // Main js file
 
-
 import buttonLike from '../components/button-like/button-like';
-import Dropdown from '../components/dropdown/dropdown';
+import { Dropdown } from '../components/dropdown/dropdown';
 import checkboxListOpen from '../components/expandable-checkbox-list/expandable-checkbox-list';
 import { headerLink, burgerMenu } from '../components/header/header';
 import rangeSlider from '../components/range-slider/range-slider';
 import Slider from '../components/room-card/room-card';
 import makeChart from '../components/chart/chart';
+import { dropdownTypes } from '../components/dropdown/dropdown-types';
 
-
-
-let sliders = document.querySelectorAll('.room-card__slider');
-const dropdownItems = document.querySelectorAll('.dropdown');
+const sliders = document.querySelectorAll('.room-card__slider');
+const dropdownGuestElement = document.querySelector('.js-guest');
+const dropdownComfortElement = document.querySelector('.js-comfort');
+const { guests, rooms } = dropdownTypes;
 
 if (sliders) {
-  sliders.forEach(slider => new Slider(slider).init());
+  sliders.forEach((slider) => new Slider(slider).init());
 }
 
-burgerMenu()
+burgerMenu();
 buttonLike();
-//dropdownItems.forEach(elem => new Dropdown(elem).init());
+try {
+  const dropdownGuest = new Dropdown(dropdownGuestElement, guests);
+  const dropdownComfort = new Dropdown(dropdownComfortElement, rooms);
+  dropdownGuest.init();
+  dropdownComfort.init();
+} catch (error) {}
+
 checkboxListOpen();
 headerLink();
-makeChart({goodCount: 65, greatCount: 130, satisfactorilyCount: 65, bad: 0});
-
-
+makeChart({ goodCount: 65, greatCount: 130, satisfactorilyCount: 65, bad: 0 });
