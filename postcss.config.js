02cfb1ffbbc9ea4614eac9e7d82241c5d72f4f93
Kampaku <43931529+kampaku@users.noreply.github.com>
2021-11-24 10:50:33 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,9 @@
-// autoprefixer - https://github.com/postcss/autoprefixer
-// css-mqpacker - https://github.com/hail2u/node-css-mqpacker
-// cssnano      - https://github.com/hail2u/node-css-mqpacker
-
-// npm install postcss-loader autoprefixer css-mqpacker cssnano --save-dev
-
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('css-mqpacker'),
-    require('cssnano')({
-      preset: [
-        'default', {
-          discardComments: {
-            removeAll: true
-          }
-        }
-      ]
-    })
-  ]
+  syntax: 'postcss-scss',
+  parser: 'postcss-scss',
+  plugins: {
+    'postcss-preset-env': {
+      browsers: 'last 2 versions',
+    },
+  },
 }
