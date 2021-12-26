4e1a870aa7c6a69ba86f2a66c4fdeab0b1f0bec7
Kampaku <43931529+kampaku@users.noreply.github.com>
2021-12-26 16:51:37 +0300

    Update colors-types.js

--- src/pug/pages/colors-types/colors-types.js
+++ src/pug/pages/colors-types/colors-types.js
@@ -1,2 +1,7 @@
-import './colors&types.scss';
-import '~components/logo/logo';
+import '~scss/main.scss';
+
+import 'components/logo/logo';
+import 'components/color-example/color-example';
+import 'components/type-example/type-example';
+
+import './colors-types.scss';
