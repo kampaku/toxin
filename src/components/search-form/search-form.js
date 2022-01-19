753836f08d3073773be94ec2197e1955f2a0e6d9
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-19 12:56:35 +0300

    Update search-form.js

--- src/components/search-form/search-form.js
+++ src/components/search-form/search-form.js
@@ -1,15 +1,18 @@
-import './search-form.scss';
 import '~components/button/button';
-import '~components/date-dropdown/DateDropdown';
-import '~components/dropdown/Dropdown';
 import { dropdownTypes } from '~components/dropdown/dropdown-types';
 import { Dropdown } from '~components/dropdown/Dropdown';
 import { DateDropdown } from '~components/date-dropdown/DateDropdown';
+
+import './search-form.scss';
 
 const searchForm = document.querySelector('.js-search-form');
 const dropdownGuestElement = searchForm && searchForm.querySelector('.js-guest');
 const dateDropdown = searchForm && searchForm.querySelector('.js-date-dropdown');
 const { guests } = dropdownTypes;
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
 
 if (dropdownGuestElement) {
   new Dropdown(dropdownGuestElement, guests)
