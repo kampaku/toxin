3747b3b4cd95bb24471b8069a3cb5c91a04b40d6
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-19 12:57:45 +0300

    Update reservation-form.js

--- src/components/reservation-form/reservation-form.js
+++ src/components/reservation-form/reservation-form.js
@@ -1,15 +1,18 @@
+import 'components/button/button';
+import { DateDropdown } from 'components/date-dropdown/DateDropdown';
+import { dropdownTypes } from 'components/dropdown/dropdown-types';
+import { Dropdown } from 'components/dropdown/Dropdown';
+
 import './reservation-form.scss';
-import '~components/button/button';
-import '~components/date-dropdown/DateDropdown';
-import '~components/dropdown/Dropdown';
-import { DateDropdown } from '~components/date-dropdown/DateDropdown';
-import { dropdownTypes } from '~components/dropdown/dropdown-types';
-import { Dropdown } from '~components/dropdown/Dropdown';
 
 const reservationForm = document.querySelector('.js-reservation-form');
 const dateDropdown = reservationForm && reservationForm.querySelector('.js-date-dropdown');
 const dropdownGuestElement = reservationForm && reservationForm.querySelector('.js-guest');
 const { guests } = dropdownTypes;
+
+reservationForm.addEventListener('submit', e => {
+  e.preventDefault();
+})
 
 if (dateDropdown) {
   new DateDropdown(dateDropdown);
