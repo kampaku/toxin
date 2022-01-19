6a41600e08b1b9c93eead0218e1dc11e50f7e6fc
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-19 23:36:56 +0300

    create mobile footer block

--- src/pages/headers-footers/headers-footers.js
+++ src/pages/headers-footers/headers-footers.js
@@ -3,5 +3,6 @@ import '~scss/main.scss';
 import 'components/logo/logo';
 import 'components/footer/footer';
 import 'components/header/header';
+import 'components/mobile-footer/mobile-footer';
 
 import './headers-footers.scss';
