cac7f96a621f6e5c9e5116e3c8fabe93049a3da8
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-20 18:56:46 +0300

    add form for subscription field #3

--- src/components/subscription-field/subscription-field.js
+++ src/components/subscription-field/subscription-field.js
@@ -1,1 +1,6 @@
 import './subscription-field.scss';
+
+const subscriptionField = document.querySelector('.js-subscription-field');
+
+subscriptionField.addEventListener('submit', e => e.preventDefault());
+
