779acca2b33c0d7b8d1da97552b628ae33754b14
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-24 18:08:43 +0300

    convert function to class

--- src/components/header-menu/header-menu.js
+++ src/components/header-menu/header-menu.js
@@ -1,13 +1,23 @@
 import './header-menu.scss';
 
 const submenus = document.querySelectorAll('.js-header-menu__link_expand');
-function openSubmenu() {
-  if (!submenus) return;
-  submenus.forEach(menu => {
-    menu.addEventListener('click', () => {
-      menu.nextElementSibling.classList.toggle('header-menu__submenu_active');
+
+class Submenu {
+  constructor(submenus) {
+    this.submenus = submenus;
+    this.toggle = this.toggle.bind(this);
+    this.bindListener();
+  }
+
+  toggle(menu) {
+    menu.nextElementSibling.classList.toggle('header-menu__submenu_active');
+  }
+
+  bindListener() {
+    this.submenus.forEach(menu => {
+      menu.addEventListener('click', () => this.toggle(menu));
     })
-  })
+  }
 }
 
-openSubmenu();
+new Submenu(submenus);
