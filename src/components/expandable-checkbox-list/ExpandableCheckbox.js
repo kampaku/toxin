170b2ebafdb3f987fb657aa059caacf14389e7c9
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-26 18:45:19 +0300

    fix naming

--- src/components/expandable-checkbox-list/ExpandableCheckbox.js
+++ src/components/expandable-checkbox-list/ExpandableCheckbox.js
@@ -2,16 +2,16 @@ import './expandable-checkbox-list.scss';
 
 export default class ExpandableCheckbox {
   constructor(elem) {
-    this.container = elem.querySelector('.js-expandable-checkbox__wrapper');
-    this.list = elem.querySelector('.js-expandable-checkbox__items');
-    this.img = elem.querySelector('.js-expandable-checkbox__button-expand');
+    this.container = elem.querySelector('.js-expandable-checkbox-list__wrapper');
+    this.list = elem.querySelector('.js-expandable-checkbox-list__items');
+    this.img = elem.querySelector('.js-expandable-checkbox-list__button-expand');
     this.init();
   }
 
   init() {
     this.container.addEventListener('click', () => {
-      this.list.classList.toggle('expandable-checkbox__items_active');
-      this.img.classList.toggle('expandable-checkbox__button-expand_active');
+      this.list.classList.toggle('expandable-checkbox-list__items_active');
+      this.img.classList.toggle('expandable-checkbox-list__button-expand_active');
     })
   }
 }
