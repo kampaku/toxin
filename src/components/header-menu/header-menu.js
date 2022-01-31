070d633cce217d422f9999fa1a4c95ce8ff90c89
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-01-31 21:27:54 +0300

    refactor header menu

--- src/components/header-menu/header-menu.js
+++ src/components/header-menu/header-menu.js
@@ -1,23 +1,46 @@
 import './header-menu.scss';
 
-const submenus = document.querySelectorAll('.js-header-menu__link_expand');
+const headerMenu = document.querySelector('.js-header-menu');
 
-class Submenu {
-  constructor(submenus) {
-    this.submenus = submenus;
-    this.toggle = this.toggle.bind(this);
+
+class HeaderMenu {
+  constructor(element) {
+    this.headerMenu = element;
+    this.expandMenus = element.querySelectorAll('.js-header-menu__link_expand');
+    this.expand = false;
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.closeAll = this.closeAll.bind(this);
+    this.closeListener = this.closeListener.bind(this);
     this.bindListener();
   }
 
-  toggle(menu) {
-    menu.nextElementSibling.classList.toggle('header-menu__submenu_active');
+  closeAll() {
+    this.expandMenus.forEach(menu => {
+      menu.nextElementSibling.classList.remove('header-menu__submenu_active');
+    });
+    document.removeEventListener('click', this.closeListener);
+  }
+
+  closeListener({ target }) {
+    if (target.matches('.js-header-menu__link_expand')) return;
+    this.closeAll();
+  }
+
+  toggleMenu({ target }) {
+    if (!target.matches('.js-header-menu__link_expand')) return;
+    const submenu = target.nextElementSibling;
+    if (submenu.classList.contains('header-menu__submenu_active')) {
+      target.nextElementSibling.classList.remove('header-menu__submenu_active');
+    } else {
+      this.closeAll();
+      target.nextElementSibling.classList.add('header-menu__submenu_active');
+      document.addEventListener('click', this.closeListener);
+    }
   }
 
   bindListener() {
-    this.submenus.forEach(menu => {
-      menu.addEventListener('click', () => this.toggle(menu));
-    })
+    this.headerMenu.addEventListener('click', this.toggleMenu);
   }
 }
 
-new Submenu(submenus);
+new HeaderMenu(headerMenu);
