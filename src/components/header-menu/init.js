dc08876fa1dd390274060d694146e99023e44a14
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-02-16 19:02:52 +0300

    Update init.js

--- src/components/header-menu/init.js
+++ src/components/header-menu/init.js
@@ -1,9 +1,8 @@
 import HeaderMenu from './HeaderMenu';
-
 import './header-menu.scss';
 
 const headerMenus = document.querySelectorAll('.js-header-menu');
 
 headerMenus.forEach(menu => {
   new HeaderMenu(menu);
-})
+});
